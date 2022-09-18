1fc378dd63e9149e0759a216f809e7367173260a
Mateus Henrique <mateushcandido5@gmail.com>
2022-09-18 18:50:51 -0300

    project created

---
+++ src/test/java/com/product/service/TomorrowTecApplicationTests.java
@@ -0,0 +1,12 @@
+package com.product.service;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomorrowTecApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
